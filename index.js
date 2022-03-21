67e4a7fb76615c9faaedcfe6159c6a88085029a9
Vtourk <85363914+Vtourk@users.noreply.github.com>
2022-03-21 21:35:24 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const aiPlayer = 'X';
 
 //List out all the possible winning combinations of cells 
 const winCombos = [
-	[0, 1, 2],
-	[3, 4, 5],
+	[0, 1, 3],
+	[2, 4, 5],
 	[6, 7, 8],
 	[0, 3, 6],
 	[1, 4, 7],
-	[2, 5, 8],
+	[2, 3, 8],
 	[0, 4, 8],
 	[6, 4, 2]
 ]
